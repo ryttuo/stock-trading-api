171b4807578dca1fa48b4ce63e8c41c2c15b5a66
Andres Solano <andres.solano@gmail.com>
2025-05-29 13:37:53 -0600

    added stocks entity

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { Logger } from '@nestjs/common';
+import { VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  const globalPrefix = 'api';
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+  app.setGlobalPrefix(globalPrefix);
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 bootstrap();
